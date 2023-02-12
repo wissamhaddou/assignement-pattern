a78e6ed0dafc6b5e737b4e69f54a673b4031621b
wissam <wissamhaddou95@gmail.com>
2023-02-12 13:04:27 -0600

    first commit

---
+++ Pattern1.java
@@ -0,0 +1,23 @@
+package assignementpattern;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		
+		
+
+		  
+		  
+		  for (int i = 1;i<=5 ; i++) {
+			  for (int j=1 ; j<=i ;j++) {
+		  
+		  System.out.print(" ");
+		  }
+		  for (int j=i ; j<=5 ; j++) {
+		  
+		  System.out.print("*");
+		  }
+		  System.out.println();
+			}
+		}
+		}
---
+++ Pattern2.java
@@ -0,0 +1,24 @@
+package assignementpattern;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		for (int i=1 ;i<=5 ; i++) {
+			
+			  
+			for (int j= i ; j<=5 ; j++) {
+				   
+				   System.out.print(" ");
+			   }
+					   
+		             for (int j= 1 ; j<=i ; j++) {
+				   
+				  System.out.print("*"); 
+			   }
+			System.out.println();
+			   }
+				}
+		}
